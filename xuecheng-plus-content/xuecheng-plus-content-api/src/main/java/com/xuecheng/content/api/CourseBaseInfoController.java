84b2addcc598ef6ec233966e24565d2e6f5987ea
lw9916 <1473381720@qq.com>
2023-06-24 23:13:40 +0800

    补充idea自带的http test

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "课程信息管理接口", tags = "课程信息管理接口")
 @RestController // 响应json数据，相当于@controller和responseBody
 public class CourseBaseInfoController {
+
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
 
 
     @ApiOperation("课程查询接口")
@@ -22,7 +27,8 @@ public class CourseBaseInfoController {
     // require = false 让无条件查询也可成立，即json也可为空
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
 
-        return null;
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+        return courseBasePageResult;
     }
 
 }
